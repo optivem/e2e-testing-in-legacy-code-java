580f7eee35bf279d2f30ab49ccb583874fb6b883
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-22 17:59:36 +0100

    Update smoke test command to accept environment and external system modes; rename ShopSmokeTest class

--- system-test/smoke-test/src/test/java/com/optivem/eshop/systemtest/smoketests/ShopSmokeTest.java
+++ system-test/smoke-test/src/test/java/com/optivem/eshop/systemtest/smoketests/ShopSmokeTest.java
@@ -1,13 +1,11 @@
-package com.optivem.eshop.systemtest.smoketests.stub;
+package com.optivem.eshop.systemtest.smoketests;
 
-import com.optivem.eshop.systemtest.EnvironmentMode;
 import com.optivem.eshop.systemtest.SystemDslFactory;
-import com.optivem.eshop.systemtest.core.ExternalSystemMode;
 import com.optivem.eshop.systemtest.core.SystemDsl;
-import com.optivem.testing.channels.Channel;
-import com.optivem.testing.channels.ChannelExtension;
 import com.optivem.eshop.systemtest.core.shop.ChannelType;
 import com.optivem.lang.Closer;
+import com.optivem.testing.channels.Channel;
+import com.optivem.testing.channels.ChannelExtension;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,6 +34,7 @@ public class ShopSmokeTest {
                 .execute()
                 .shouldSucceed();
     }
+
     @Test
     void shouldBeAbleToGoToErp() {
         app.erp().goToErp()
