2e227d0933020a573446528b5b72189631584223
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-26 12:08:45 +0100

    Refactor SuccessVerificationBuilder to enhance order number prefix verification; support ViewOrderVerification and improve type handling

--- dsl/gherkin/src/main/java/com/optivem/eshop/systemtest/core/gherkin/then/SuccessVerificationBuilder.java
+++ dsl/gherkin/src/main/java/com/optivem/eshop/systemtest/core/gherkin/then/SuccessVerificationBuilder.java
@@ -1,6 +1,7 @@
 package com.optivem.eshop.systemtest.core.gherkin.then;
 
 import com.optivem.eshop.systemtest.core.shop.dsl.verifications.PlaceOrderVerification;
+import com.optivem.eshop.systemtest.core.shop.dsl.verifications.ViewOrderVerification;
 import com.optivem.testing.dsl.ResponseVerification;
 import com.optivem.testing.dsl.UseCaseContext;
 
@@ -14,9 +15,14 @@ public class SuccessVerificationBuilder<TVerification extends ResponseVerificati
     }
 
     public SuccessVerificationBuilder<TVerification> expectOrderNumberPrefix(String prefix) {
-        if (successVerification instanceof PlaceOrderVerification) {
-            ((PlaceOrderVerification) successVerification).orderNumberStartsWith(prefix);
+        if (successVerification instanceof PlaceOrderVerification verification) {
+            verification.orderNumberStartsWith(prefix);
+        } else if(successVerification instanceof ViewOrderVerification verification) {
+            verification.orderNumberHasPrefix(prefix);
+        } else {
+            throw new IllegalStateException("expectOrderNumberPrefix is not supported for this verification type");
         }
+
         return this;
     }
 
